41a115d156c7ab9cd87817628469a8a4f4d9120f
Dhurges Pandiarajan <dhurgeswaran@gmail.com>
2021-03-04 01:53:17 +0530

    Added Callback hell code

---
+++ callback_hell/script.js
@@ -0,0 +1,36 @@
+var counter = 11;
+setTimeout(
+    ()=>{counter--;console.log(counter); 
+        setTimeout(
+            ()=>{counter--;console.log(counter); 
+                setTimeout(
+                    ()=>{counter--;console.log(counter); 
+                        setTimeout(
+                            ()=>{counter--;console.log(counter); 
+                                setTimeout(
+                                    ()=>{counter--;console.log(counter); 
+                                        setTimeout(
+                                            ()=>{counter--;console.log(counter); 
+                                                setTimeout(
+                                                    ()=>{counter--;console.log(counter); 
+                                                        setTimeout(
+                                                            ()=>{counter--;console.log(counter); 
+                                                                setTimeout(
+                                                                    ()=>{counter--;console.log(counter); 
+                                                                        setTimeout(
+                                                                            ()=>{counter--;console.log(counter);
+                                                                                setTimeout(
+                                                                                    ()=>{console.log("Happy Independence Day"); 
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);    
+},1000);           
+    
+    
